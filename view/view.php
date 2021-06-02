29e343a0554b343a297361cde6ccf3c9240a74e9
Laura Daniela Chiquillo Leon <lchiquillo@unbosque.edu.co>
2021-06-02 13:08:14 -0500

    cambio vista

--- view/view.php
+++ view/view.php
@@ -1,38 +1,10 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-    <title></title>
+<meta charset="UTF-8">
+<title>INICIO</title>
 </head>
 <body>
-    <h1 align="center">LISTADO DE MEDICOS</h1>
-    <table width="70%" border="1px" align="center">
-
-    <tr align="center">
-        <td>Codigo</td>
-        <td>Identificacion</td>
-        <td>Nombre</td>
-        <td>Apellidos</td>
-        <td>Especialidad</td>
-        <td>Telefono</td>
-        <td>Correo</td>
-    </tr>
-    <?php 
-        while($datos=$resultado->fetch_array()){
-        ?>
-            <tr>
-                <td><?php echo $datos["idMedico"]?></td>
-                <td><?php echo $datos["medIdentificacion"]?></td>
-                <td><?php echo $datos["medNombres"]?></td>
-                <td><?php echo $datos["medApellidos"]?></td>
-                <td><?php echo $datos["medEspecialidad"]?></td>
-                <td><?php echo $datos["medTelefono"]?></td>
-                <td><?php echo $datos["medCorreo"]?></td>
-            </tr>
-            <?php   
-        }
-
-     ?>
-    </table>
-
+    <button type="submit">Enviar</button>
 </body>
 </html>
