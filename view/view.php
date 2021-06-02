48137f826b96e4ba0abab361fdd64e0ed21b225a
Laura Daniela Chiquillo Leon <lchiquillo@unbosque.edu.co>
2021-06-02 15:14:03 -0500

    vista

--- view/view.php
+++ view/view.php
@@ -5,6 +5,14 @@
 <title>INICIO</title>
 </head>
 <body>
-    <button type="submit">Enviar</button>
+    <!-- seleccionar un archivo-->
+    <form enctype="multipart/form-data" action="" name="form" method="post">
+        Seleccione Archivo
+            <input type="file" name="file" id="file" /></td>
+            <input type="submit" name="submit" id="submit" value="Subir" />
+    </form>
+    <h1>
+    <table></table>
+    </h1>
 </body>
 </html>
