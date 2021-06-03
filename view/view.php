209c764e41d1e477ccc78d687904b7c8a0f1f857
Laura Daniela Chiquillo Leon <lchiquillo@unbosque.edu.co>
2021-06-03 02:06:04 -0500

    conección

--- view/view.php
+++ view/view.php
@@ -1,3 +1,42 @@
+<?php 
+
+// conexión
+$mysqli = @new mysqli('localhost',"laura","tisoruno123", "proyecto");
+
+if (isset($_POST['enviar']))
+{
+	
+  $filename=$_FILES["file"]["name"];
+  $info = new SplFileInfo($filename);
+  $extension = pathinfo($info->getFilename(), PATHINFO_EXTENSION);
+
+   if($extension == 'csv')
+   {
+	$filename = $_FILES['file']['tmp_name'];
+	$handle = fopen($filename, "r");
+
+	while( ($data = fgetcsv($handle, 1000, ";") ) !== FALSE )
+	{
+	   $q = "INSERT INTO importacion (codigo, codigoSede, nombres, apellidos, fechaIngreso, sueldo, arl) VALUES (
+        '$data[0]', 
+        '$data[1]',
+        '$data[2]',
+        '$data[3]', 
+        '$data[4]',
+        '$data[5]',
+        '$data[6]'
+    )";
+
+	$mysqli -> query($q);
+    #echo "se subio";
+        #$mysql_query($q);
+   }
+
+      fclose($handle);
+   }
+}
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,14 +44,11 @@
 <title>INICIO</title>
 </head>
 <body>
+    
     <form enctype="multipart/form-data" method="post" action="">
     CSV File:<input type="file" name="file" id="file">
     <input type="submit" value="Enviar" name="enviar">
     </form>
-    <?php
-    $control = new Controller();
-    $control -> conectar();
-    ?>
     <h1>
     <table></table>
     </h1>
