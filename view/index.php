8ce4db154ac68b6d78b6c67dcc5643871ca190e5
Laura Daniela Chiquillo Leon <lchiquillo@unbosque.edu.co>
2021-06-13 17:54:13 -0500

    s

--- view/index.php
+++ view/index.php
@@ -5,13 +5,50 @@
 <title>INICIO</title>
 </head>
 <body>
-<header>
-    <form enctype="multipart/form-data" method="post" action="">
-    <input type="submit" value="pdf" name="pdf">
-    </form>
-    </header>
-    <?php
-    echo "<td><a href='../controller/pdf.php'> <button type='button' class= 'btn btn-outline-success'>Acta PDF</button></a></td>";
-    ?>
+    <table id="theTable" class="display" border="1" style="width: 50%">
+    <tfoot>
+        <tr>
+            <th>empleados por ciudad y sucursal.</th>
+            <th>
+                <?php
+                echo "<td><a href='../controller/pdf.php'> <button type='button' class= 'btn btn-outline-success'>PDF</button></a></td>";
+                ?>
+            </th>
+        </tr>
+        <tr>
+            <th>Sumatoria de sueldos pagados por sucursal en un período determinado.</th>
+            <th> 
+                <?php
+                echo "<td><a href='../controller/consulta2.php'> <button type='button' class= 'btn btn-outline-success'>PDF</button></a></td>";
+                ?>
+            </th>
+        </tr>
+        <tr>
+            <th>empleados que presente su código, nombre, sueldo, descuento por pensión (es el 4% del valor del sueldo), bonificación y total recibido.</th>
+            <th> 
+                <?php
+                echo "<td><a href='../controller/consulta3.php'> <button type='button' class= 'btn btn-outline-success'>PDF</button></a></td>";
+                ?>
+            </th>
+        </tr>
+        <tr>
+            <th>empleados en un rango de fechas que hayan tenido alguna novedad.</th>
+            <th> 
+                <?php
+                echo "<td><a href='../controller/consulta4.php'> <button type='button' class= 'btn btn-outline-success'>PDF</button></a></td>";
+                ?>
+            </th>
+        </tr>
+        <tr>
+            <th></th>
+            <th> 
+                <?php
+                echo "<td><a href='../controller/consulta5.php'> <button type='button' class= 'btn btn-outline-success'>PDF</button></a></td>";
+                ?>
+            </th>
+        </tr>
+    </tfoot>
+</table>
+<a  href="login.php"> Login</a>
 </body>
 </html>
